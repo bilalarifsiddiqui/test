4c70fcd65fc7c0756d603dd235f8fa8d88c72c1e
bilalarifsiddiqui <bilal.arif.siddiqui@gmail.com>
2020-01-03 12:26:52 +0500

    Updates

---
+++ precache-manifest.7ae998a4736d24f9e75dae6f3ef6f19e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0ef37a38ff7423ccee180ddb40426e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "bcd6f2b4bbb9b0a43f1c",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aa098c5bad7cc13e9243",
+    "url": "/test/static/js/2.ed69aa69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.ed69aa69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcd6f2b4bbb9b0a43f1c",
+    "url": "/test/static/js/main.9457a7fe.chunk.js"
+  },
+  {
+    "revision": "d9d290c5ce56573893b9",
+    "url": "/test/static/js/runtime-main.2769e43a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
